5f94ffe38614b61c57d36abfc3c63e4055411d28
Murli03 <murlibhatnagar03@gmail.com>
2021-01-16 13:43:19 +0530

    update

---
+++ module4solution/script.js
@@ -0,0 +1,19 @@
+	var n= new Array();
+		n[0]="Yakkov";
+		n[1]="john";
+		n[2]="jen";
+		n[3]="jason";
+		n[4]="paul";
+		n[5]="Frank";
+		n[6]="lauran";
+		n[7]="jim"
+for (i = 0; i <n.length; i++) {
+	if(n[i].charAt(0)==='J' || n[i].charAt(0)==='j'){
+		console.log("Goodbye "+n[i] )
+	}
+	else{
+		console.log("Hello "+n[i] )
+		 
+	}
+ 
+}
